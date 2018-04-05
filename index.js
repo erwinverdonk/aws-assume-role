d36757bbcfdbfc4f3c6d2d9dfac6df68f0565796
Erwin Verdonk <e.verdonk@gmail.com>
2018-04-05 15:37:08 +0200

    Replace dbl quotes with single + indenting + remove direct applying of credentials

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const storage = require("node-persist");
-const readline = require("readline");
-const AWS = require("aws-sdk");
+const storage = require('node-persist');
+const readline = require('readline');
+const AWS = require('aws-sdk');
 
 const requestMFACode = () => {
   return new Promise((resolve, reject) => {
@@ -9,7 +9,7 @@ const requestMFACode = () => {
       output: process.stdout
     });
 
-    rl.question("Please enter MFA code: ", code => {
+    rl.question('Please enter MFA code: ', code => {
       if (code.length !== 6) {
         return requestMFACode();
       }
@@ -23,36 +23,26 @@ const requestMFACode = () => {
 exports.assumeRole = params => {
   const durationSeconds = params.durationSeconds || 3600; // 1 hour
 
-  if (!params.region) {
-    throw new Error("No region provided.");
-  }
-
   if (!params.roleArn) {
-    throw new Error("No Role ARN provided.");
+    throw new Error('No Role ARN provided');
   }
 
-  const region = params.region;
+  const region = params.region || process.env.AWS_DEFAULT_REGION;
   const roleArn = params.roleArn;
 
   storage.initSync({ ttl: durationSeconds * 1000 });
 
-  const AWS_ACCESS_KEY_ID = storage.getItemSync("AWS_ACCESS_KEY_ID");
-  const AWS_SECRET_ACCESS_KEY = storage.getItemSync("AWS_SECRET_ACCESS_KEY");
-  const AWS_SESSION_TOKEN = storage.getItemSync("AWS_SESSION_TOKEN");
+  const AWS_ACCESS_KEY_ID = storage.getItemSync('AWS_ACCESS_KEY_ID');
+  const AWS_SECRET_ACCESS_KEY = storage.getItemSync('AWS_SECRET_ACCESS_KEY');
+  const AWS_SESSION_TOKEN = storage.getItemSync('AWS_SESSION_TOKEN');
 
   if (AWS_ACCESS_KEY_ID && AWS_SECRET_ACCESS_KEY && AWS_SESSION_TOKEN) {
-    process.env.AWS_ACCESS_KEY_ID = AWS_ACCESS_KEY_ID;
-    process.env.AWS_SECRET_ACCESS_KEY = AWS_SECRET_ACCESS_KEY;
-    process.env.AWS_SESSION_TOKEN = AWS_SESSION_TOKEN;
-
-    AWS.config = new AWS.Config({ region });
-
     return Promise.resolve({
       roleArn,
       credentials: {
-        AccessKeyId: AWS_ACCESS_KEY_ID,
-        SecretAccessKey: AWS_SECRET_ACCESS_KEY,
-        SessionToken: AWS_SESSION_TOKEN
+        accessKeyId: AWS_ACCESS_KEY_ID,
+        secretAccessKey: AWS_SECRET_ACCESS_KEY,
+        sessionToken: AWS_SESSION_TOKEN
       }
     });
   }
@@ -84,14 +74,14 @@ exports.assumeRole = params => {
         return sts
           .assumeRole({
             RoleArn: roleArn,
-            RoleSessionName: "aws-assume-role",
+            RoleSessionName: 'aws-assume-role',
             SerialNumber: device.SerialNumber,
             TokenCode: mfa.code,
             DurationSeconds: durationSeconds
           })
           .promise()
           .catch(_ => {
-            if (_.code === "AccessDenied" && mfa.devices.length > 0) {
+            if (_.code === 'AccessDenied' && mfa.devices.length > 0) {
               return tryToAssume(mfa.devices.pop());
             } else {
               throw _;
@@ -102,18 +92,16 @@ exports.assumeRole = params => {
       return tryToAssume(mfa.devices.pop());
     })
     .then(_ => {
-      return { roleArn, credentials: _.Credentials };
+      return { roleArn, credentials: {
+        accessKeyId: _.Credentials.AccessKeyId,
+        secretAccessKey: _.Credentials.SecretAccessKey,
+        sessionToken: _.Credentials.SessionToken
+      }};
     })
     .then(_ => {
-      process.env.AWS_ACCESS_KEY_ID = _.credentials.AccessKeyId;
-      process.env.AWS_SECRET_ACCESS_KEY = _.credentials.SecretAccessKey;
-      process.env.AWS_SESSION_TOKEN = _.credentials.SessionToken;
-
-      AWS.config = new AWS.Config();
-
-      storage.setItemSync("AWS_ACCESS_KEY_ID", _.credentials.AccessKeyId);
-      storage.setItemSync("AWS_SECRET_ACCESS_KEY", _.credentials.SecretAccessKey);
-      storage.setItemSync("AWS_SESSION_TOKEN", _.credentials.SessionToken);
+      storage.setItemSync('AWS_ACCESS_KEY_ID', _.credentials.accessKeyId);
+      storage.setItemSync('AWS_SECRET_ACCESS_KEY', _.credentials.secretAccessKey);
+      storage.setItemSync('AWS_SESSION_TOKEN', _.credentials.sessionToken);
 
       return _;
     });
