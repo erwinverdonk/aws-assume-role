34bb0ab6169d80d9d4ae3ea9322c85270fa3cfd2
Erwin Verdonk <e.verdonk@gmail.com>
2018-04-06 12:01:42 +0200

    Refactor to make more robust and better readable code

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const storage = require('node-persist');
 const readline = require('readline');
 const AWS = require('aws-sdk');
 
-const requestMFACode = () => {
-  return new Promise((resolve, reject) => {
+const requestMFA = () => {
+  const promptForMFACode = () => new Promise((resolve, reject) => {
     const rl = readline.createInterface({
       input: process.stdin,
       output: process.stdout
@@ -11,98 +11,106 @@ const requestMFACode = () => {
 
     rl.question('Please enter MFA code: ', code => {
       if (code.length !== 6) {
-        return requestMFACode();
+        return promptForMFACode();
       }
 
       resolve(code);
       rl.close();
     });
   });
-};
-
-exports.assumeRole = params => {
-  const durationSeconds = params.durationSeconds || 3600; // 1 hour
-
-  if (!params.roleArn) {
-    throw new Error('No Role ARN provided');
-  }
-
-  const region = params.region || process.env.AWS_DEFAULT_REGION;
-  const roleArn = params.roleArn;
-
-  storage.initSync({ ttl: durationSeconds * 1000 });
-
-  const AWS_ACCESS_KEY_ID = storage.getItemSync('AWS_ACCESS_KEY_ID');
-  const AWS_SECRET_ACCESS_KEY = storage.getItemSync('AWS_SECRET_ACCESS_KEY');
-  const AWS_SESSION_TOKEN = storage.getItemSync('AWS_SESSION_TOKEN');
-
-  if (AWS_ACCESS_KEY_ID && AWS_SECRET_ACCESS_KEY && AWS_SESSION_TOKEN) {
-    return Promise.resolve({
-      roleArn,
-      credentials: {
-        accessKeyId: AWS_ACCESS_KEY_ID,
-        secretAccessKey: AWS_SECRET_ACCESS_KEY,
-        sessionToken: AWS_SESSION_TOKEN
-      }
-    });
-  }
 
   return new AWS.IAM()
     .listMFADevices()
     .promise()
     .then(_ => _.MFADevices || [])
-    .then(devices =>
-      new Promise((resolve, reject) => {
-        if (devices.length === 0) {
-          resolve({ code: undefined, devices: [] });
-          return;
-        }
+    .then(devices => new Promise((resolve, reject) => {
+      if (devices.length === 0) {
+        resolve({ code: undefined, devices: [] });
+        return;
+      }
 
-        requestMFACode()
-          .then(code => {
-            resolve({
-              code,
-              devices
-            });
-          })
-          .catch(_ => reject(_));
-      })
-    )
-    .then(mfa => {
-      const sts = new AWS.STS();
-      const tryToAssume = device => {
-        return sts
-          .assumeRole({
-            RoleArn: roleArn,
-            RoleSessionName: 'aws-assume-role',
-            SerialNumber: device.SerialNumber,
-            TokenCode: mfa.code,
-            DurationSeconds: durationSeconds
-          })
-          .promise()
-          .catch(_ => {
-            if (_.code === 'AccessDenied' && mfa.devices.length > 0) {
-              return tryToAssume(mfa.devices.pop());
-            } else {
-              throw _;
-            }
+      promptForMFACode()
+        .then(code => {
+          resolve({
+            code,
+            devices
           });
-      };
-
-      return tryToAssume(mfa.devices.pop());
+        })
+        .catch(_ => reject(_));
     })
-    .then(_ => {
-      return { roleArn, credentials: {
-        accessKeyId: _.Credentials.AccessKeyId,
-        secretAccessKey: _.Credentials.SecretAccessKey,
-        sessionToken: _.Credentials.SessionToken
-      }};
-    })
-    .then(_ => {
-      storage.setItemSync('AWS_ACCESS_KEY_ID', _.credentials.accessKeyId);
-      storage.setItemSync('AWS_SECRET_ACCESS_KEY', _.credentials.secretAccessKey);
-      storage.setItemSync('AWS_SESSION_TOKEN', _.credentials.sessionToken);
+  );
+};
 
-      return _;
-    });
+const getStoredCredentials = () => {
+  const accessKeyId = storage.getItemSync('AWS_ACCESS_KEY_ID');
+  const secretAccessKey = storage.getItemSync('AWS_SECRET_ACCESS_KEY');
+  const sessionToken = storage.getItemSync('AWS_SESSION_TOKEN');
+
+  return Promise.resolve(
+    accessKeyId && secretAccessKey && sessionToken
+      ? { accessKeyId, secretAccessKey, sessionToken }
+      : undefined
+  );
+};
+
+const setStoredCredentials = credentials => {
+  storage.setItemSync('AWS_ACCESS_KEY_ID', credentials.accessKeyId);
+  storage.setItemSync('AWS_SECRET_ACCESS_KEY', credentials.secretAccessKey);
+  storage.setItemSync('AWS_SESSION_TOKEN', credentials.sessionToken);
+
+  return Promise.resolve(credentials);
+};
+
+const requestTemporaryCredentials = (roleArn, durationSeconds) => requestMFA()
+  .then(mfa => {
+    const sts = new AWS.STS();
+    const tryToAssume = device => {
+      return sts
+        .assumeRole({
+          RoleArn: roleArn,
+          RoleSessionName: 'aws-assume-role',
+          SerialNumber: device.SerialNumber,
+          TokenCode: mfa.code,
+          DurationSeconds: durationSeconds
+        })
+        .promise()
+        .catch(_ => {
+          if (_.code === 'AccessDenied' && mfa.devices.length > 0) {
+            return tryToAssume(mfa.devices.pop());
+          } else {
+            throw _;
+          }
+        });
+    };
+
+    return tryToAssume(mfa.devices.pop());
+  })
+  .then(_ => ({
+    credentials: {
+      accessKeyId: _.Credentials.AccessKeyId,
+      secretAccessKey: _.Credentials.SecretAccessKey,
+      sessionToken: _.Credentials.SessionToken
+    }
+  }))
+  .then(_ => {
+    return setStoredCredentials(_.credentials);
+  });
+
+exports.assumeRole = params => {
+  if (!params.roleArn) {
+    throw new Error('No Role ARN provided');
+  }
+
+  const durationSeconds = params.durationSeconds || 3600; // 1 hour
+  const region = params.region || process.env.AWS_DEFAULT_REGION;
+  const roleArn = params.roleArn;
+
+  if (!region) {
+    throw new Error('No region provided');
+  }
+
+  storage.initSync({ ttl: durationSeconds * 1000 });
+
+  return getStoredCredentials()
+    .then(_ => !_ ? requestTemporaryCredentials(roleArn, durationSeconds) : _);
 };
